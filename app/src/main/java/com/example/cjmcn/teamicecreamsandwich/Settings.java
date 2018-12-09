b26534ffb5e06939c2fca8e420de496e5bc7f8c1
barnesm04 <mhbarnes@umich.edu>
2018-12-09 15:38:46 -0500

    Added buttons and text boxes on signup page

--- app/src/main/java/com/example/cjmcn/teamicecreamsandwich/Settings.java
+++ app/src/main/java/com/example/cjmcn/teamicecreamsandwich/Settings.java
@@ -2,12 +2,18 @@ package com.example.cjmcn.teamicecreamsandwich;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Settings extends Activity {
+public class Settings extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
