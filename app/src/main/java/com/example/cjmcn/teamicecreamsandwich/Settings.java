cda506ebec0884819a1920adbbba0464af6afb0b
cocomcnally <cjmcn@umich.edu>
2018-12-14 16:33:53 -0500

    Added Functionality:
    Create Events
    Edit Events
    Search for event by type on create page (not detailed search)
    
    Added Eventstorage class for the firebase calls

--- app/src/main/java/com/example/cjmcn/teamicecreamsandwich/Settings.java
+++ app/src/main/java/com/example/cjmcn/teamicecreamsandwich/Settings.java
@@ -1,19 +1,57 @@
 package com.example.cjmcn.teamicecreamsandwich;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Settings extends Activity implements View.OnClickListener{
+
+    Button  buttonInterests, buttonSecutrity;
+    Button  buttonFAQ, buttonLegal, buttonAbout, buttonUsername;
+    Button  buttonHomepage;
+    TextView    textViewSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        buttonInterests = findViewById(R.id.buttonInterests);
+        buttonSecutrity = findViewById(R.id.buttonSecurity);
+        buttonFAQ = findViewById(R.id.buttonFAQ);
+        buttonLegal = findViewById(R.id.buttonLegal);
+        buttonAbout = findViewById(R.id.buttonAbout);
+        buttonUsername = findViewById(R.id.buttonUsername);
+        buttonHomepage = findViewById(R.id.buttonHomepage);
+        textViewSettings= findViewById(R.id.textViewSettings);
+
+
+        buttonInterests.setOnClickListener(this);
+        buttonSecutrity.setOnClickListener(this);
+        buttonFAQ.setOnClickListener(this);
+        buttonLegal.setOnClickListener(this);
+        buttonAbout.setOnClickListener(this);
+        buttonUsername.setOnClickListener(this);
+        buttonHomepage.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
 
+        if(v == buttonHomepage){
+            Intent intentHomePage = new Intent(Settings.this, HomePage.class);
+            startActivity(intentHomePage);
+        }
+
+
+
     }
 }
+
--- app/src/main/java/com/example/cjmcn/teamicecreamsandwich/Signup.java
+++ app/src/main/java/com/example/cjmcn/teamicecreamsandwich/Signup.java
@@ -1,11 +1,22 @@
 package com.example.cjmcn.teamicecreamsandwich;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signup extends Activity implements View.OnClickListener{
 
@@ -13,6 +24,8 @@ public class Signup extends Activity implements View.OnClickListener{
     EditText    editTextEnterCurrentLocation, editTextEnterEmail, editTextEnterPassword, editTextConfirmPassword;
     TextView    textViewPrivacy;
     Button      buttonSignUp;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +44,60 @@ public class Signup extends Activity implements View.OnClickListener{
         textViewPrivacy = findViewById(R.id.textViewPrivacy);
 
         buttonSignUp.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     @Override
     public void onClick(View v) {
 
+        if (v == buttonSignUp) {
+            mAuth.createUserWithEmailAndPassword(editTextEnterUserName.getText().toString(), editTextEnterPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(Signup.this, "Welcome", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(Signup.this, "Access Denied", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+        } /*else if(v == buttonLogIn) {
+
+            mAuth.signInWithEmailAndPassword(editTextEnterUserName.getText().toString(), editTextEnterPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser currentUser = mAuth.getCurrentUser();
+                                Toast.makeText(Signup.this, "Login Successful ", Toast.LENGTH_SHORT).show();
+                                Intent intentMainScreen = new Intent(Signup.this, HomePage.class);
+                                startActivity(intentMainScreen);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(Signup.this, "Access Denied", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+        }*/
+
+
     }
+
+
+
 }
+
+
